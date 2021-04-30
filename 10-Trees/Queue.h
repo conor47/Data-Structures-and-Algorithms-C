70f285084c2b26bbc5954bc1725f8fbec4fa2179
Conor Ladrigan <Conorladrigan95@gmail.com>
2021-04-30 22:41:35 +0100

    added tree exercises

---
+++ 10-Trees/Queue.h
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+
+    struct Node *lchild;
+    int data;
+    struct Node *rchild;
+};
+
+struct Queue {
+
+    int size;
+    int front;
+    int rear;
+    struct Node **Q;
+};
+
+void Create(struct Queue *q, int size){
+
+    q->size = size;
+    q->front = q->rear = 0;
+    q->Q = (struct Node **) malloc (q->size * sizeof(struct Node *));
+}   
+
+void Enqueue (struct Queue *q,struct Node * x){
+
+    if ((q->rear+1)%q->size == q->front ){
+        
+        printf("queue is full");
+    }
+
+    else {
+
+        q->rear = (q->rear+1)%q->size;
+        q->Q[q->rear]= x;
+    }
+
+}
+
+struct Node * Dequeue (struct Queue *q){
+
+    struct Node * x=NULL;
+
+    if(q->front == q->rear){
+
+        printf("Queue is empty\n");
+    }
+    else {
+        q->front = (q->front+1)%q->size ;
+        x = q->Q[q->front];
+    }
+
+    return x;
+}
+
+int isEmpty (struct Queue q){
+
+    return q.front == q.rear;
+}
